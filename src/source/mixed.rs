073d29cc6be00cd2178a57570d3abcea5f294ef1
Eduard Müller <mail@emuell.net>
2022-09-03 21:42:42 +0200

    use lock free queue instead of a mutex guarded list in mixer

--- src/source/mixed.rs
+++ src/source/mixed.rs
@@ -1,69 +1,108 @@
-use std::sync::Mutex;
-
-use super::{mapped::ChannelMappedSource, resampled::ResampledSource, AudioSource};
+use super::AudioSource;
 use crate::utils::resampler::ResamplingQuality;
+use crossbeam_queue::SegQueue;
 
 // -------------------------------------------------------------------------------------------------
 
+struct PlayingSource {
+    is_active: bool,
+    source: Box<dyn AudioSource>,
+}
+
+// -------------------------------------------------------------------------------------------------
+
+#[derive(Default)]
 pub struct MixedSource {
-    sources: Mutex<Vec<Box<dyn AudioSource>>>,
+    pending_sources: SegQueue<Box<dyn AudioSource>>,
+    sources: Vec<PlayingSource>,
     channel_count: usize,
     sample_rate: u32,
     temp_out: Vec<f32>,
 }
 
 impl MixedSource {
-    pub fn new() -> Self {
-        const BUFFER_SIZE: usize = 1024; // TODO: get audio sink's buffer size here
+    pub fn new(channel_count: usize, sample_rate: u32) -> Self {
+        const BUFFER_SIZE: usize = 8 * 1024;
         Self {
-            sources: Mutex::new(Vec::new()),
-            channel_count: 2,
-            sample_rate: 48000,
+            pending_sources: SegQueue::new(),
+            sources: Vec::new(),
+            channel_count,
+            sample_rate,
             temp_out: vec![0.0; BUFFER_SIZE],
         }
     }
 
-    pub fn add(&self, source: Box<dyn AudioSource>) {
-        let mut converted_source: Box<dyn AudioSource> = source;
-        if converted_source.sample_rate() != self.sample_rate() {
+    pub fn add<T>(&self, source: T)
+    where
+        T: AudioSource,
+    {
+        // add to pending_sources which will be consumed in the next write
+        if source.sample_rate() != self.sample_rate() {
             // convert source sample-rate to ours
-            converted_source = Box::new(ResampledSource::new(
-                converted_source,
-                self.sample_rate(),
-                ResamplingQuality::SincMediumQuality,
-            ));
-        }
-        if converted_source.channel_count() != self.channel_count() {
+            let source = source.resampled(self.sample_rate(), ResamplingQuality::SincMediumQuality);
+            if source.channel_count() != self.channel_count() {
+                let source = source.channel_mapped(self.channel_count());
+                self.pending_sources.push(Box::new(source));
+            } else {
+                self.pending_sources.push(Box::new(source));
+            }
+        } else if source.channel_count() != self.channel_count() {
             // convert source channel mapping to ours
-            converted_source = Box::new(ChannelMappedSource::new(
-                converted_source,
-                self.channel_count(),
-            ));
+            let source = source.channel_mapped(self.channel_count());
+            self.pending_sources.push(Box::new(source));
+        } else {
+            self.pending_sources.push(Box::new(source));
         }
-        self.sources.lock().unwrap().push(converted_source);
     }
 }
 
 impl AudioSource for MixedSource {
     fn write(&mut self, output: &mut [f32]) -> usize {
-        output.iter_mut().for_each(|v| *v = 0f32);
-
+        let channel_count = self.channel_count();
+        // clear output as we're only adding below
+        for o in output.iter_mut() {
+            *o = 0_f32;
+        }
+        // add pending sources
+        while let Some(source) = self.pending_sources.pop() {
+            self.sources.push(PlayingSource {
+                is_active: true,
+                source,
+            });
+        }
+        // write all sources
         let mut max_written = 0usize;
-        for source in self.sources.lock().unwrap().iter_mut() {
-            let to_write = output.len();
+        for playing_source in self.sources.iter_mut() {
+            let source = &mut playing_source.source;
+            debug_assert_eq!(
+                channel_count,
+                source.channel_count(),
+                "expecting same channel layout"
+            );
             let mut total_written = 0;
-            while total_written < to_write {
-                let remaining = to_write - total_written;
-                let temp_out_size = remaining.min(self.temp_out.len() - 1);
-                let written = source.write(&mut self.temp_out[..temp_out_size]);
+            'source: while total_written < output.len() {
+                // run source on temp_out until we've filled up the whole final output
+                let remaining = output.len() - total_written;
+                let to_write = remaining.min(self.temp_out.len());
+                let written = source.write(&mut self.temp_out[..to_write]);
+                if written == 0 {
+                    // no more samples present in this source
+                    playing_source.is_active = false;
+                    break 'source;
+                }
+                // add output of the source to the final output
                 let remaining_out = &mut output[total_written..];
-                for (i, v) in self.temp_out[..written].iter_mut().enumerate() {
-                    remaining_out[i] += *v;
+                let written_out = &self.temp_out[..written];
+                for (o, i) in remaining_out.iter_mut().zip(written_out) {
+                    *o += *i;
                 }
                 total_written += written;
             }
             max_written = max_written.max(total_written);
         }
+        // remove stopped sources
+        self.sources.retain(|s| s.is_active);
+        // return dirty output len
         max_written
     }
 
