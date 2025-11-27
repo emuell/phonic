e957634923c3d6bdd7bbaacaa7e59237e1013eb1
Eduard Müller <mail@emuell.net>
2025-11-27 21:41:01 +0100

    update play-sequence example
    - use samplers instead of plain file sources - stop & resume playback while scheduling notes

--- examples/play-sequence.rs
+++ examples/play-sequence.rs
@@ -3,7 +3,11 @@
 
 use std::time::Duration;
 
-use phonic::{sources::PreloadedFileSource, utils::speed_from_note, Error, FilePlaybackOptions};
+use phonic::{
+    sources::generators::Sampler,
+    utils::{ahdsr::AhdsrParameters, speed_from_note},
+    Error,
+};
 
 // -------------------------------------------------------------------------------------------------
 
@@ -26,18 +30,39 @@ fn main() -> Result<(), Error> {
     // Create a player instance with the output device as configured via program arguments
     let mut player = arguments::new_player(&args, None)?;
 
-    // Preload samples
-    let cowbell = PreloadedFileSource::from_file(
-        "assets/cowbell.wav",
+    // Stop playback until we've scheduled all notes
+    player.stop();
+
+    // Create samplers
+    let metronome = player.play_generator_source(
+        Sampler::from_file(
+            "assets/cowbell.wav",
+            None,
+            None,
+            2,
+            player.output_channel_count(),
+            player.output_sample_rate(),
+        )?,
         None,
-        Default::default(),
-        player.output_sample_rate(),
+        None,
     )?;
-    let bass = PreloadedFileSource::from_file(
-        "assets/bass.wav",
+    let bass = player.play_generator_source(
+        Sampler::from_file(
+            "assets/bass.wav",
+            Some(AhdsrParameters::new(
+                Duration::from_millis(10),
+                Duration::ZERO,
+                Duration::ZERO,
+                1.0,
+                Duration::from_secs_f32(2.0),
+            )?),
+            None,
+            4,
+            player.output_channel_count(),
+            player.output_sample_rate(),
+        )?,
         None,
-        FilePlaybackOptions::default(),
-        player.output_sample_rate(),
+        None,
     )?;
 
     // Sequencer timing
@@ -47,80 +72,64 @@ fn main() -> Result<(), Error> {
 
     let samples_per_sec = player.output_sample_rate();
     let samples_per_beat = (60.0 / BPM * samples_per_sec as f64) as u64;
-    let samples_per_bar = BEATS_PER_BAR as u64 * samples_per_beat;
 
-    // Start 1 second ahead of the current playback time
     let output_start_time = player.output_sample_frame_position() + samples_per_sec as u64;
-    let mut current_time = output_start_time;
 
     // Schedule metronome beats
+    let mut current_time = output_start_time;
     for beat in 0..(BEATS_PER_BAR * BARS_TO_PLAY) {
-        let sample_time = current_time + beat as u64 * samples_per_beat;
-        let speed = speed_from_note(if beat.is_multiple_of(BEATS_PER_BAR) {
-            72
-        } else {
-            60
-        });
-        player
-            .play_file_source(
-                cowbell.clone(FilePlaybackOptions::default().speed(speed), samples_per_sec)?,
-                sample_time,
-            )?
-            .stop(sample_time + samples_per_beat)?;
+        let note = match beat {
+            _ if beat.is_multiple_of(BEATS_PER_BAR) => 72,
+            _ => 60,
+        };
+        metronome.note_on(note, Some(1.0), None, current_time)?;
+        current_time += samples_per_beat;
     }
+    // Stop sampler at the end of the sequence
+    metronome.stop(current_time)?;
 
     // Schedule bass line with glides (midi_note, duration_in_beats, glide, volume, pan)
     let bass_line = [
         (60, 4.0, None, None, Some(0.0)),
-        (56, 1.0, Some(999.0), Some(0.75), Some(1.0)),
-        (58, 1.0, Some(999.0), Some(0.5), Some(-1.0)),
+        (56, 1.0, Some(999.0), Some(0.75), Some(0.8)),
+        (58, 1.0, Some(999.0), Some(0.5), Some(-0.8)),
         (65, 2.0, Some(12.0), None, Some(0.0)),
         (56, 4.0, Some(96.0), Some(1.0), None),
     ];
 
-    // Play first note
-    let (first_note, first_duration_beats, _glide, volume, panning) = bass_line[0];
-    let bass_playback_handle = player.play_file_source(
-        bass.clone(
-            FilePlaybackOptions::default()
-                .speed(speed_from_note(first_note))
-                .volume(volume.unwrap_or(1.0))
-                .panning(panning.unwrap_or(0.0))
-                .fade_out(Duration::from_millis(1000)),
-            samples_per_sec,
-        )?,
-        current_time,
-    )?;
+    // Start bass with the first metronome beat
+    current_time = output_start_time;
+    let mut bass_note_id = None;
 
-    // Schedule subsequent speed changes for the bass line
-    let first_duration_samples = (first_duration_beats * samples_per_beat as f64) as u64;
-    current_time += first_duration_samples;
-
-    for (note, duration_beats, glide, volume, panning) in &bass_line[1..] {
-        bass_playback_handle.set_speed(speed_from_note(*note), *glide, current_time)?;
-        if let Some(volume) = volume {
-            bass_playback_handle.set_volume(*volume, current_time)?;
+    for (note, beats, glide, volume, panning) in &bass_line {
+        match bass_note_id {
+            // Glide existing note
+            Some(bass_note_id) if glide.is_some() => {
+                bass.set_note_speed(bass_note_id, speed_from_note(*note), *glide, current_time)?;
+                if let Some(volume) = volume {
+                    bass.set_note_volume(bass_note_id, *volume, current_time)?;
+                }
+                if let Some(panning) = panning {
+                    bass.set_note_panning(bass_note_id, *panning, current_time)?;
+                }
+            }
+            // Play new note
+            _ => {
+                bass_note_id = Some(bass.note_on(*note, *volume, *panning, current_time)?);
+            }
         }
-        if let Some(panning) = panning {
-            bass_playback_handle.set_panning(*panning, current_time)?;
-        }
-        let duration_samples = (duration_beats * samples_per_beat as f64) as u64;
-        current_time += duration_samples;
+        current_time += (*beats * samples_per_beat as f64) as u64;
     }
+    // Stop sampler at the end of the metronome sequence
+    bass.stop((BEATS_PER_BAR * BARS_TO_PLAY + 1) as u64 * samples_per_beat)?;
 
-    // Wait for sequence playback to finish
-    while player.is_running()
-        && player.output_sample_frame_position()
-            < output_start_time + BARS_TO_PLAY as u64 * samples_per_bar
-    {
+    // Start playback
+    player.start();
+
+    // Wait for playback to finish
+    while player.is_running() && (bass.is_playing() || metronome.is_playing()) {
         std::thread::sleep(Duration::from_millis(100));
     }
-
-    // Stop all sources (the bass sample will still play)
-    player.stop_all_sources()?;
-
-    // Give sources time to fade out
-    std::thread::sleep(Duration::from_secs(2));
 
     Ok(())
 }
