d7c9933871c437f5946bcdd24bfef6b52beaa4d9
Eduard Müller <mail@emuell.net>
2026-03-02 23:06:05 +0100

    update emscripten example

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 fn main() {
     let target = std::env::var("TARGET").expect("No TARGET env variable set");
-
     // inject emscripten build options
     if target.contains("emscripten") {
-        println!("cargo::rustc-link-arg=-fexceptions");
         println!("cargo::rustc-link-arg=--no-entry");
     }
 }
--- examples/play-emscripten/build.rs
+++ examples/play-emscripten/build.rs
@@ -4,7 +4,7 @@ fn main() {
     // inject emscripten build options
     if target.contains("emscripten") {
         // compile options
-        println!("cargo::rustc-link-arg=-fexceptions");
+        println!("cargo::rustc-link-arg=-fwasm-exceptions");
         println!("cargo::rustc-link-arg=-sINVOKE_RUN=0");
         println!("cargo::rustc-link-arg=-sUSE_PTHREADS=1");
         println!("cargo::rustc-link-arg=-sPTHREAD_POOL_SIZE=4");
